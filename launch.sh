25f3c388df32fddf541b70ad3a4bdcd4e4e852ab
Mike <github@coolatta.org>
2016-07-01 22:09:47 -0400

    Forcefully remove any running containers before recreating

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ if [ ! -d /home/ec2-user/mongodata ]; then
 fi
 
 # Stop any running containers
-docker rm -v apache mongodb
+docker rm -v -f apache mongodb
 
 # Launch MongoDB
 docker run -d --name mongodb -p 27017:27017 -v /home/ec2-user/mongodata:/data/db mongo:3.2.7
